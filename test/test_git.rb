851233634ae85929b9f9153b45b5560ce1eb876d
sanemat <o.gata.ken@gmail.com>
2015-03-05 03:16:23 +0900

    feat(protocol): check git/ssh protocol method

--- test/test_git.rb
+++ test/test_git.rb
@@ -7,7 +7,24 @@ module URI
         test 'myself' do
           assert do
             ::URI::SshGit.parse('git@github.com:packsaddle/ruby-uri-ssh_git.git') \
-              == Generic.build(userinfo: 'git', host: 'github.com', path: '/packsaddle/ruby-uri-ssh_git.git')
+              == Generic.build(
+                userinfo: 'git',
+                host: 'github.com',
+                path: '/packsaddle/ruby-uri-ssh_git.git'
+              )
+          end
+        end
+      end
+
+      sub_test_case '.ssh_protocol?' do
+        test 'ssh protocol' do
+          assert do
+            ::URI::SshGit.ssh_protocol?('git@github.com:packsaddle/ruby-uri-ssh_git.git')
+          end
+        end
+        test 'git protocol' do
+          assert do
+            !::URI::SshGit.ssh_protocol?('git://github.com/packsaddle/ruby-uri-ssh_git.git')
           end
         end
       end
