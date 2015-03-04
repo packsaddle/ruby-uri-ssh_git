da1efe4e268836c6d902b4ed725bc5f713d761ea
sanemat <o.gata.ken@gmail.com>
2015-03-05 03:08:12 +0900

    feat(ssh): implement ssh protocol

--- lib/uri/ssh_git.rb
+++ lib/uri/ssh_git.rb
@@ -6,7 +6,10 @@ require 'uri/ssh_git/version'
 module URI
   module SshGit
     def self.parse(uri_string)
-      Generic.build(scheme: nil)
+      host_part, path_part = uri_string.split(':', 2)
+      userinfo, host = host_part.split('@', 2)
+      path_part = '/' + path_part unless path_part.start_with?('/')
+      Generic.build(userinfo: userinfo, host: host, path: path_part)
     end
   end
 end
---
+++ test/test_git.rb
@@ -0,0 +1,16 @@
+require_relative 'helper'
+
+module URI
+  module SshGit
+    class TestGit < Test::Unit::TestCase
+      sub_test_case '.parse' do
+        test 'myself' do
+          assert do
+            ::URI::SshGit.parse('git@github.com:packsaddle/ruby-uri-ssh_git.git') \
+              == Generic.build(userinfo: 'git', host: 'github.com', path: '/packsaddle/ruby-uri-ssh_git.git')
+          end
+        end
+      end
+    end
+  end
+end
