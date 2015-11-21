4baea68ef59048b4f1be8609f014095426e3a528
sanemat <o.gata.ken@gmail.com>
2015-11-22 00:15:07 +0900

    docs(yard): add document

--- lib/uri/ssh_git.rb
+++ lib/uri/ssh_git.rb
@@ -4,7 +4,29 @@ require 'uri/ssh_git/generic'
 require 'uri/ssh_git/version'
 
 module URI
+  # Parse and build git repository url via ssh protocol.
   module SshGit
+    # @example
+    #   url = URI::SshGit.parse('git@github.com:packsaddle/ruby-uri-ssh_git.git')
+    #   => #<URI::SshGit::Generic git@github.com:packsaddle/ruby-uri-ssh_git.git>
+    #
+    #   url.scheme #=> nil
+    #   url.userinfo #=> 'git'
+    #   url.user #=> 'git'
+    #   url.password #=> nil
+    #   url.host #=> 'github.com'
+    #   url.port #=> nil
+    #   url.registry #=> nil
+    #   url.path #=> '/packsaddle/ruby-uri-ssh_git.git'
+    #   url.opaque #=> nil
+    #   url.query #=> nil
+    #   url.fragment #=> nil
+    #
+    # @see http://docs.ruby-lang.org/en/2.2.0/URI/Generic.html
+    #
+    # @param url [String] git repository url via ssh protocol
+    #
+    # @return [Generic] parsed object
     def self.parse(uri_string)
       host_part, path_part = uri_string.split(':', 2)
       userinfo, host = host_part.split('@', 2)
--- lib/uri/ssh_git/generic.rb
+++ lib/uri/ssh_git/generic.rb
@@ -1,6 +1,16 @@
 module URI
   module SshGit
+    # @see http://docs.ruby-lang.org/en/2.2.0/URI/Generic.html
     class Generic < ::URI::Generic
+      # @example
+      #   URI::SshGit.build(
+      #     userinfo: 'git',
+      #     host: 'github.com',
+      #     path: '/packsaddle/ruby-uri-ssh_git.git'
+      #   ).to_s
+      #   => 'git@github.com:packsaddle/ruby-uri-ssh_git.git'
+      #
+      # @return [String] git repository url via ssh protocol
       def to_s
         show_path = path.slice(1..-1) if path.start_with?('/')
         "#{user}@#{host}:#{show_path}"
